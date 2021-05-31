0e8cf8e515b1a583a675d9296d31d921dc3074e9
Nycolas De Lima Vieira <nyvieira@mbpdemeliadmin.lan>
2021-05-31 17:12:54 -0300

    create structure

---
+++ src/main/java/br/com/digitalhouse/bootcamp/sprintchallenge/SprintChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.digitalhouse.bootcamp.sprintchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintChallengeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/digitalhouse/bootcamp/sprintchallenge/controllers/UserController.java
@@ -0,0 +1,19 @@
+package br.com.digitalhouse.bootcamp.sprintchallenge.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping
+    public ResponseEntity<String> metadata() {
+
+        return new ResponseEntity("teste", HttpStatus.OK);
+    }
+
+}
